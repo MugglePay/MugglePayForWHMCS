4f15cb395213a4a775f4e91bdb446cc7094291a4
BitPay Dev - Bitcoin payment supported <50819254+safepayment@users.noreply.github.com>
2020-01-06 15:01:58 +0800

    Make default as USDT

--- modules/gateways/bitpayxForCrypto.php
+++ modules/gateways/bitpayxForCrypto.php
@@ -41,6 +41,7 @@ function bitpayxForCrypto_link($params) {
         'merchant_order_id' => 'WHMCS_' . $params['invoiceid'],
         'price_amount' => $params['amount'],
         'price_currency' => 'CNY',
+        'pay_currency' => 'USDT',
         'title' => '支付单号：' . $params['invoiceid'],
         'description' => '充值：' . $params['amount'] . ' 元',
         'callback_url' => $systemurl."modules/gateways/bitpayxForCrypto/notify.php",
